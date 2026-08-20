122cce4102958ef2655cc3bd55788cf1b69ca07e
Tushar Kashyape <tusharkashyape8@gmail.com>
2026-08-21 00:25:13 +0530

    Topic wise Interview Qs and Quick Hands-on files

--- Python/1. Lists/memory_allocation.py
+++
@@ -1,9 +0,0 @@
-import sys
-a = []
-prev = sys.getsizeof(a)
-for i in range(20):
-    a.append(i)
-    size = sys.getsizeof(a)
-    if size != prev:
-        print(f"len={len(a)}: size jumped to {size} bytes")
-        prev = size
