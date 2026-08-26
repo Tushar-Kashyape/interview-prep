480a0a12b682868f141423c2877092ee822766a1
Tushar Kashyape <tusharkashyape8@gmail.com>
2026-08-26 18:35:30 +0530

    [ DSA ] - 26/08 - Linked List - LC - Delet Node by position - DONE

---
+++ Daily Code (DSA, Python, np + pandas)/DSA/3. Linked Lists/1. delete_node.py
@@ -0,0 +1,23 @@
+from linked_list import Node
+
+def delete_node(head: Node, pos) -> Node:
+    if pos < 0 or not head:
+        raise IndexError(f"{pos} is invalid")
+
+    if pos == 0:
+        new_head = head.next
+        head.next = None
+        return new_head
+
+    temp, prev, count = head, None, 0
+    while temp and count != pos:
+        prev = temp
+        temp = temp.next
+        count += 1
+
+    if temp:
+        prev.next = temp.next
+        temp.next = None
+        return head
+    else:
+        raise IndexError(f"Index {pos} out of bounds of linked list")
