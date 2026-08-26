d1443917968ffb8156c2a6eb01fb78ffd0ba8da3
Tushar Kashyape <tusharkashyape8@gmail.com>
2026-08-26 12:49:01 +0530

    [ Python ] - 26/08 - OOPs(classes, objects) basic code - Done

---
+++ Daily Code (DSA, Python, np + pandas)/Python/OOPs/1. classes/1. classes, objects.py
@@ -0,0 +1,36 @@
+class Room:
+
+    def __init__(self, room_number: str, price_per_night: float):
+        self.room_number = room_number
+        self.price_per_night = price_per_night
+        self.is_booked = False
+
+    def book_room(self):
+        self.is_booked = True
+
+    def calculate_cost(self, nights: int) -> float:
+        return self.price_per_night * nights
+
+
+room_num = input("Enter room number: ")
+price = int(input("Enter price per night: "))
+
+room = Room(room_number=room_num, price_per_night=price)
+
+nights = int(input("Enter number of nights for which room to be booked: "))
+total_price = room.calculate_cost(nights=nights)
+
+print(f"\n[Booked] Room {room.room_number} at {room.price_per_night}/ night")
+print(f"Total price for {nights}: {total_price}")
+
+"""
+O/p:
+
+Enter room number: 301
+Enter price per night: 2300
+Enter number of nights for which room to be booked: 3
+
+[Booked] Room 301 at 2300/ night
+Total price for 3: 6900
+"""
+
